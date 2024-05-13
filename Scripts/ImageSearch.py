81497db80c893d2d960d5d0c2e1de2f8b8b0c95e
100495680 <100495680@alumnos.uc3m.es>
2024-05-13 19:49:18 +0200

    Image Search Improved

--- Scripts/ImageSearch.py
+++ Scripts/ImageSearch.py
@@ -1,21 +1,11 @@
-import requests
-from urllib.parse import urlencode
-from bs4 import BeautifulSoup
-import tkinter as tk
-from PIL import Image, ImageTk
-import os
-
-
-# Example usage
-query = "Jeremie Frimpong"
-
-
-images = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAIXM7r0oB_aqs_1dOaq6t_WgxthzN8LlzmEFq__By2yoUvoAEoNEXVcAutQ&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsa_G8Ke7eGmRxPE8gkwOj7TdJ2mWTJx36zg4tDSTEtMLJSAlX632AkW9qWw&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6RGBVbXJeLardmLMqpVJ4aFoM5jSffTeDSn4CzDh8YivYBMK0zYdcbwIKg6w&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYJd_mzNO4TO8r50rznnmvw027xqpCkKXAxB0743fBlNLClMbTq1CA4UWf_Q&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC5f6CGA6sFn6FIvk5BbES1p2HwAW8nQ2NheW4hdCPOItg1cZ2xIAgWE2in5A&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwuceAsyJluAps6RWsL3Yx8ymbRvj1NU95K-SGiQ13_VdlIK7y1t5qEL6jFQ&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH4nnbPdjsrB8g3mm3RMeOfYjRj54JjUsFkQpFK9GpIk_yspeTlpK7CivzEcw&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM1CmSCDFWCmMhhw0TL_X_IhsEachzmQ40yTbvFwh2Ib4kzdYzkN_GUymGug&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2gqw7UsXowRFlRemEMkwvL2DwSnPC1SyMoma1VIZost8JE9YHmG-mOhaFig&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZtEBTU2zQkDWxY47XeVA4fabgz8L6spseCM-nU4Go2q8vNhS1MDdY6-r3Vg&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5MYWDYGOszStBhocY2ZXo69C8_ycPyiUlX19R23qyob1HBBoyH6XGNIEMKw&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPKdLgJ-sULpU-uO3pN_pfByM-RyC1YD_56rltK2GUP4rirAsOibMltuXhza4&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaF6zB0DLQIy1S0Nw7KSCgb3cXi0mVYegyHCc4jup_zcCGZqQECYYaeOHwShs&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtGlocNKr0Ib_b8gq9J3lNWGj9PS6SLvJDTNGZCTNfjh3lgQiVQxLHVeanCw&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcOFl3crxDUeDoKtBS7CFxrDpZx8WxKhAdPAK7uxMl9xzTFRIhK6KshUjHSi4&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT21Lk7YkENL1jsB_uOS8F7Nesf72zMN3uDws16laKwykQpqsWlUOkLl9BIoRk&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpe9MwVUSzx21ov0Q2LTtKufj1uOzakqhz_ZXngu-dPJxAybJ50uXAfK_b34A&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkrWuL2Pomq7wbfATSI-ir0JL-ZXmhO7K1IT-xbDkK2Y3HHmaQHFmEL7bEjAs&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIQR5aHaQbkiWaMh8iXAzznZjU419amc1co1lAjV5UsbVRVj94oyvDObORNA&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE-lwo6wthdZ--UUNUFJ9JlBVkh1Uk4M2ULvJm0v4FCCP41OAK2h559NFq5A&s']
-
 import tkinter as tk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+import os
+from urllib.parse import urlencode
+from bs4 import BeautifulSoup
+import shutil
 
 class ImageGridApp:
     def __init__(self, root, query):
@@ -23,8 +13,11 @@ class ImageGridApp:
         self.root.title("Image Grid Viewer")
 
         self.image_urls, self.real_urls = self.google_image_search(query) 
-
+        self.visited_urls = []
         self.create_image_grid()
+
+        self.close_button = tk.Button(self.root, text="Close", command=self.close_and_move_images)
+        self.close_button.grid(row=len(self.image_urls) // 5 + 1, columnspan=5, pady=10)
 
     def google_image_search(self, query):
         # Define the base URL
@@ -98,14 +91,31 @@ class ImageGridApp:
         response = requests.get(self.real_urls[index])
         soup = BeautifulSoup(response.text, 'html.parser')
         image_results = soup.find_all('img')
-        images_downladed = len([image for image in os.listdir("C:/Users/ignac/Documents/Documentos/Football/Futty Data/Automation Code/Template/Code/Video Output") if 'photo' in image])
+        path = "C:/Users/ignac/Documents/Documentos/Football/Futty Data/Automation Code/Template/Code/Video Output"
+        images_downloaded = len([image for image in os.listdir(path) if 'photo' in image])
         image_url = image_results[0]['src']
         image = requests.get(image_url)
-        with open(f"image_{images_downladed+1}.jpg", "wb") as f:
+        with open(f"{path}/image_{images_downloaded+1}.jpg", "wb") as f:
             f.write(image.content)
-        print(f"Image {images_downladed+1} downloaded successfully!")
+        print(f"Image {images_downloaded+1} downloaded successfully!")
+
+    def close_and_move_images(self):
+        images = [image for image in os.listdir("C:/Users/ignac/Documents/Documentos/Football/Futty Data/Automation Code/Template/Code/Video Output") if 'photo' in image]
+        for image in images:
+            source_folder = "C:/Users/ignac/Documents/Documentos/Football/Futty Data/Automation Code/Template/Code/Video Output"
+            output_folder = "C:/Users/ignac/Documents/Documentos/Football/Futty Data/Automation Code/Template/Code/images"
+            shutil.move(source_folder+image,output_folder)
+        root.destroy()
 
 if __name__ == "__main__":
     root = tk.Tk()
+    query =  "Jeremie Frimpong" # Replace "your_query_here" with your desired query
+    app = ImageGridApp(root, query)
+    root.mainloop()
+
+
+def ImageSearch(player):
+    root = tk.Tk()
+    query =  "Jeremie Frimpong" # Replace "your_query_here" with your desired query
     app = ImageGridApp(root, query)
     root.mainloop()
